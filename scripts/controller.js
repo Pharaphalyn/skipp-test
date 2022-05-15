fa4e9cb9cb9d91adc80b8c1fa83a2e8582a87fab
Tudrug <108qwl@gmail.com>
2022-05-15 19:56:53 +0300

    Add links to rects

--- scripts/controller.js
+++ scripts/controller.js
@@ -1,5 +1,10 @@
 let selectedElement, offset, transform, svg;
 rectsList = [];
+linesList = [];
+linksList = [];
+
+
+const xmlns = "http://www.w3.org/2000/svg";
 
 const defaultRect = {
     id: null,
@@ -10,7 +15,17 @@ const defaultRect = {
     stroke: '#000',
     strokeWidth: '0.02', 
     fill: 'transparent',
-    new: true
+    new: true,
+    type: 'rect'
+};
+
+const defaultLink = {
+    id: null,
+    x: null,
+    y: null,
+    fill: null,
+    r: 0.1,
+    parent: null
 };
 
 const defaultLine = {
@@ -19,7 +34,7 @@ const defaultLine = {
     end: null,
     startSide: null,
     endSide: null,
-    links: []
+    type: 'line'
 }
 
 function makeDraggable(evt) {
@@ -33,13 +48,46 @@ function makeDraggable(evt) {
 }
 
 function createRect(model) {
-    const xmlns = "http://www.w3.org/2000/svg";
     const newRect = Object.assign({}, model);
     newRect.id = Math.random().toString(36).slice(2);
     const rect = document.createElementNS(xmlns, "rect");
     setRectAttributes(rect, newRect);
     svg.appendChild(rect);
+    createLinksForRect(newRect);
     rectsList.push(newRect);
+}
+
+function createLinksForRect(model) {
+    getRectLinkCoordinates(model).forEach(coord => {
+        const link = Object.assign({}, defaultLink);
+        link.id = Math.random().toString(36).slice(2);
+        link.parent = model.id;
+        link.x = coord[0];
+        link.y = coord[1];
+        link.fill = model.stroke;
+        const circle = document.createElementNS(xmlns, "circle");
+        setLinkAttributes(circle, link);
+        svg.appendChild(circle);
+        linksList.push(link);
+    });
+}
+
+function setLinkAttributes(circle, link) {
+    circle.setAttribute('id', link.id);
+    circle.setAttribute('r', link.r);
+    circle.setAttribute('fill', link.fill);
+    circle.setAttribute('class', 'link');
+    setLinkCoordinates(circle, link);
+}
+
+function setLinkCoordinates(circle, link) {
+    circle.setAttribute('cx', link.x);
+    circle.setAttribute('cy', link.y);
+}
+
+function getRectLinkCoordinates(model) {
+    return [[model.x + model.width / 2, model.y], [ model.x + model.width, model.y + model.height / 2],
+            [model.x + model.width / 2, model.y + model.height], [model.x, model.y + model.height / 2]];
 }
 
 function setRectAttributes(rect, newRect) {
@@ -56,6 +104,12 @@ function setRectAttributes(rect, newRect) {
 function updateCoordinates(rect, model) {
     rect.setAttributeNS(null, 'x', model.x);
     rect.setAttributeNS(null, 'y', model.y);
+    const linkCoords = getRectLinkCoordinates(model);
+    linksList.filter(el => el.parent === model.id).forEach((link, index) => {
+        link.x = linkCoords[index][0];
+        link.y = linkCoords[index][1];
+        setLinkCoordinates(document.getElementById(link.id), link);
+    });
 }
 
 function startDrag(evt) {
